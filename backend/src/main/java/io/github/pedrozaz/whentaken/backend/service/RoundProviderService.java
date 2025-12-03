e0653eb2d3c8de38e943cf627793626f15ccebf3
Pedrozaz <guilhermebarb0sa@proton.me>
2025-12-03 20:38:58 -0300

    feat: backend now uses dataset.json for rounds

--- backend/src/main/java/io/github/pedrozaz/whentaken/backend/service/RoundProviderService.java
+++ backend/src/main/java/io/github/pedrozaz/whentaken/backend/service/RoundProviderService.java
@@ -1,32 +1,66 @@
 package io.github.pedrozaz.whentaken.backend.service;
 
 import io.github.pedrozaz.whentaken.backend.model.RoundData;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Slf4j
 @Service
-
+@RequiredArgsConstructor
 public class RoundProviderService {
 
+    private final ObjectMapper objectMapper;
+    private final List<RoundData> allRounds = new ArrayList<>();
+
+    @PostConstruct
+    public void loadRounds() {
+        try {
+            log.info("Loading rounds");
+            ClassPathResource resource = new ClassPathResource("rounds_data.json");
+
+            if (!resource.exists()) {
+                log.error("Rounds data not found");
+                return;
+            }
+
+            List<RoundData> loaded = objectMapper.readValue(
+                    resource.getInputStream(),
+                    new TypeReference<List<RoundData>>() {}
+            );
+
+            allRounds.addAll(loaded);
+            log.info("{} rounds loaded in memory.", allRounds.size());
+        } catch (IOException e) {
+            log.error("Failed to load rounds", e);
+        }
+    }
+
     public List<RoundData> getRandomRounds(int count) {
+        if (allRounds.isEmpty()) {
+            log.warn("No rounds loaded");
+            return getFallbackRounds();
+        }
 
-        List<RoundData> candidates = new ArrayList<>(getFallbackRounds());
-
-        Collections.shuffle(candidates);
-        return candidates.subList(0, Math.min(candidates.size(), count));
+        List<RoundData> shuffled = new ArrayList<>(allRounds);
+        Collections.shuffle(shuffled);
+        return  shuffled.subList(0, Math.min(count, shuffled.size()));
     }
 
     private List<RoundData> getFallbackRounds() {
         return List.of(
                 new RoundData("https://upload.wikimedia.org/wikipedia/commons/a/af/Eiffel_Tower_1900_01.jpg", 48.8584, 2.2945, 1900),
-                new RoundData("https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg", 40.7484, -73.9857, 1931),
-                new RoundData("https://upload.wikimedia.org/wikipedia/commons/3/3a/Berlin_Wall_1989_fall.jpg", 52.5163, 13.3777, 1989),
-                new RoundData("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/New_York_City_-_Times_Square_1950.jpg/800px-New_York_City_-_Times_Square_1950.jpg", 40.757, -73.986, 1950)
+                new RoundData("https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg", 40.7484, -73.9857, 1931)
         );
     }
 }
